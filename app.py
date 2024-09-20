8e754615adbbcee9e53ae1014e4fb0665c46a9eb
Philipp Rosendahl <zacc@live.de>
2024-09-20 17:44:19 +0200

    chore: Rename Marimo app

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import marimo
 
-__generated_with = "0.7.17"
-app = marimo.App(
-    width="medium",
-    app_title="ORACLE",
-    layout_file="layouts/dashboard.grid.json",
-)
+__generated_with = "0.8.15"
+app = marimo.App(width="medium", app_title="ORACLE")
 
 
 @app.cell
@@ -257,250 +253,113 @@ def __(mo):
 
 
 @app.cell
-def __(pd):
-    df_adam2014 = pd.read_pickle("data/adam2024.pkl")
-    df_vanherwijnen2016 = pd.read_pickle("data/vanherwijnen2016.pkl")
-    return df_adam2014, df_vanherwijnen2016
+def __(df_vanherwijnen_layers, propability_function, weac):
+    def cm_to_mm(row):
+        return row * 10
 
+    full_propagation = df_vanherwijnen_layers['propagation'] == True
+    slab_fracture = df_vanherwijnen_layers['slab_fracture'] == True
 
-@app.cell
-def __(df_vanherwijnen2016):
-    df_vanherwijnen2016.L.describe()
-    return
+    df_end = df_vanherwijnen_layers[full_propagation]
+    df_arr = df_vanherwijnen_layers[~full_propagation & ~slab_fracture]
+    df_sf = df_vanherwijnen_layers[~full_propagation & slab_fracture]
 
+    this_df = df_arr
+    Ewl = 0.2
+    nu = 0.25
+    Gwl = Ewl(2*(1 + nu))
 
-@app.cell
-def __(df_adam2014, df_vanherwijnen2016, pd):
-    df_err = pd.concat(
-        [
-            df_adam2014[["Gc", "GIc", "GIIc", "slope_incl"]],
-            df_vanherwijnen2016[["Gc", "GIc", "GIIc", "slope_incl"]],
-        ],
-        axis=0,
+    ac_ = this_df['rc[cm]'].apply(cm_to_mm).values
+    t_ = this_df['wl_thick[cm]'].apply(cm_to_mm).values
+    dy_ = this_df['dy[cm]'].apply(cm_to_mm).values
+    # t_[t_ > 150] = dy_[t_ > 150]  # replace thick wl with collapse height
+    phi_ = this_df['slope[degree]'].values
+
+    for i, exp in this_df.iterrows():
+        layers_cm = exp['slab_layers[thickness[cm], density[kg/m3]]']
+        layers_mm = [[hi * 10, rho] for hi, rho in layers_cm]  # Convert thickness to mm
+
+        # Initialize PST object
+        pst = weac.Layered(system='-pst', layers=layers_mm)
+
+        # Set weak-layer properties
+        pst.set_foundation_properties(t=20, E=Ewl, update=True)
+
+        # Calculate segmentation
+        segments = pst.calc_segments(phi=phi_[i], L=1000, a=ac_[i])['crack']
+
+        # Assemble model for segments and solve for free constants
+        C = pst.assemble_and_solve(phi=phi_[i], **segments)
+
+        # Calculate ERR and probability
+        Gdif = pst.gdif(C, phi_[i], **segments, unit='J/m^2')
+
+        # Calculate probability
+        prob = propability_function(Gdif[0])
+        this_df.at[i, 'probability'] = prob
+    return (
+        C,
+        Ewl,
+        Gdif,
+        Gwl,
+        ac_,
+        cm_to_mm,
+        df_arr,
+        df_end,
+        df_sf,
+        dy_,
+        exp,
+        full_propagation,
+        i,
+        layers_cm,
+        layers_mm,
+        nu,
+        phi_,
+        prob,
+        pst,
+        segments,
+        slab_fracture,
+        t_,
+        this_df,
     )
-    return df_err,
 
 
 @app.cell
-def __(df_err):
-    from uncertainties import unumpy
-    df_err['Gc_mean'] = df_err['Gc'].apply(unumpy.nominal_values)
-    df_err['Gc_std'] = df_err['Gc'].apply(unumpy.std_devs)
-    return unumpy,
+def __(t_):
+    thickwl = t_ > 150
+    return thickwl,
 
 
 @app.cell
-def __(df_err):
-    df_err.describe()
+def __(dy_, thickwl):
+    dy_[thickwl]
     return
 
 
 @app.cell
-def __(df_err):
-    import seaborn as sns
-    sns.displot(data=df_err, x="Gc_mean", kind="hist", bins = 100, aspect = 1.5)
-    return sns,
+def __(t_):
+    t_
+    return
 
 
 @app.cell
 def __():
-    from fitter import Fitter, get_common_distributions, get_distributions
-    return Fitter, get_common_distributions, get_distributions
+    from oracle.snowpilot import SnowPilotPipeline
+
+    pipeline = SnowPilotPipeline()
+    pipeline.download_recent_caaml(1)
+    return SnowPilotPipeline, pipeline
 
 
 @app.cell
-def __(get_common_distributions):
-    get_common_distributions()
+def __():
+    import oracle
+    return oracle,
+
+
+@app.cell
+def __():
     return
-
-
-@app.cell
-def __(Fitter, df_err, get_common_distributions):
-    # Create a Fitter instance, specify the data and the distributions you want to fit
-    f = Fitter(df_err['Gc_mean'], distributions=get_common_distributions())
-    # f = Fitter(df_err['Gc_mean'], distributions=get_distributions())
-
-    # Fit the data
-    f.fit()
-    return f,
-
-
-@app.cell
-def __(f):
-    # Display a summary of the results
-    f.summary(Nbest=5)
-    return
-
-
-@app.cell
-def __(f, mo, plt):
-    f.plot_pdf(Nbest=1)
-    mo.mpl.interactive(plt.gcf())
-    return
-
-
-@app.cell
-def __(f):
-    # Get the best fitting distribution and its parameters
-    best_fit = f.get_best(method='sumsquare_error')
-    print(best_fit)
-    return best_fit,
-
-
-@app.cell
-def __(df_err, np, plt, sns):
-    import scipy.stats as stats
-
-    shape, loc, scale = stats.lognorm.fit(df_err['Gc_mean'])
-
-    x = np.linspace(min(df_err['Gc_mean']), max(df_err['Gc_mean']), 1000)
-    pdf = stats.lognorm.pdf(x, shape, loc, scale)
-    cdf = stats.lognorm.cdf(x, shape, loc, scale)
-
-    plt.plot(x, pdf, lw=2, label='Probability density function', color='darkblue')
-    plt.plot(x, cdf, lw=2, label='Cumulative density function', color='darkred')
-    sns.histplot(df_err['Gc_mean'], bins=75, kde=False, stat="density", color="red",cumulative=True, label="Cumulative histogram")
-    sns.histplot(df_err['Gc_mean'], bins=75, kde=False, stat="density", color="skyblue", label="Histogram")
-    return cdf, loc, pdf, scale, shape, stats, x
-
-
-@app.cell
-def __(cdf, df_err, np, pd, pdf, x):
-    pdf_data, hist_bins = np.histogram(df_err['Gc_mean'], bins=75, density=True)
-    cdf_data = np.cumsum(pdf_data) * np.diff(hist_bins)
-    hist_x = (hist_bins[:-1] + hist_bins[1:]) / 2
-
-    hist_df = pd.DataFrame({'Gc(J/m^2)': hist_x, 'probability_density': pdf_data})
-    cum_df = pd.DataFrame({'Gc(J/m^2)': hist_x, 'probability': cdf_data})
-    pdf_df = pd.DataFrame({'Gc(J/m^2)': x, 'probability_density': pdf})
-    cdf_df = pd.DataFrame({'Gc(J/m^2)': x, 'probability': cdf})
-    probability_df = pd.DataFrame({'Gc(J/m^2)': x, 'P': 1 - cdf})
-
-    hist_df.to_csv('data/histogram.txt', index=False, sep='\t')
-    cum_df.to_csv('data/cumulative.txt', index=False, sep='\t')
-    pdf_df.to_csv('data/pdf.txt', index=False, sep='\t')
-    cdf_df.to_csv('data/cdf.txt', index=False, sep='\t')
-    probability_df.to_csv('data/probability.txt', index=False, sep='\t')
-    return (
-        cdf_data,
-        cdf_df,
-        cum_df,
-        hist_bins,
-        hist_df,
-        hist_x,
-        pdf_data,
-        pdf_df,
-        probability_df,
-    )
-
-
-@app.cell
-def __(hist_x, pdf, pdf_data, plt, x):
-    plt.bar(hist_x, pdf_data, width=.2, alpha=.3)
-    plt.plot(x, pdf)
-    return
-
-
-@app.cell
-def __(cdf, cdf_data, hist_x, plt, x):
-    plt.bar(hist_x, cdf_data, width=.2, alpha=.3)
-    plt.plot(x, cdf)
-    return
-
-
-@app.cell
-def __(df_err):
-    df_err['Gc_mean'].describe()
-    return
-
-
-@app.cell
-def __(loc, scale, shape, stats):
-    propability_function = lambda x: 1 - stats.lognorm.cdf(x, shape, loc, scale)
-    return propability_function,
-
-
-@app.cell
-def __(propability_function):
-    propability_function(1.2)
-    return
-
-
-@app.cell
-def __(np, plt, propability_function, weac):
-    # Parameters
-    phi_ = 45  # positive is downslope, negative is upslope
-    crack_lengths_ = np.arange(0, 500, 25)
-
-    plt.figure()
-
-    for profile_ in ['A', 'B', 'C', 'D', 'E', 'F', 'H']:
-
-        P_ = []
-
-        for a_ in crack_lengths_:
-
-            # Initialize PST object
-            pst_ = weac.Layered(system='-pst', layers=profile_)
-
-            # Set weak-layer properties
-            pst_.set_foundation_properties(t=20, E=0.15, update=True)
-
-            # Calculate segmentation
-            segments_ = pst_.calc_segments(phi=phi_, L=1000, a=a_)['crack']
-
-            # Assemble model for segments and solve for free constants
-            C_ = pst_.assemble_and_solve(phi=phi_, **segments_)
-
-            # Calculate ERR and probability
-            Gdif_ = pst_.gdif(C_, phi_, **segments_, unit='J/m^2')
-            P_.append(propability_function(Gdif_[0]))
-
-        plt.plot(crack_lengths_, P_, label=profile_)
-
-    plt.legend()
-    plt.show()
-    # Calculate probability
-    # propability_function(Gdif_[0])
-    return (
-        C_,
-        Gdif_,
-        P_,
-        a_,
-        crack_lengths_,
-        phi_,
-        profile_,
-        pst_,
-        segments_,
-    )
-
-
-@app.cell
-def __(propability_function, weac):
-    # Parameters
-    phi = -45  # positive is downslope, negative is upslope
-    t = 20
-    E = 0.15
-    a = 500
-
-    # Initialize PST object
-    pst = weac.Layered(system='-pst', layers='H')
-
-    # Set weak-layer properties
-    pst.set_foundation_properties(t=20, E=0.15, update=True)
-
-    # Calculate segmentation
-    segments = pst.calc_segments(phi=phi, L=1000, a=a)['crack']
-
-    # Assemble model for segments and solve for free constants
-    C = pst.assemble_and_solve(phi=phi, **segments)
-
-    # Calculate ERR and probability
-    Gdif = pst.gdif(C, phi, **segments, unit='J/m^2')
-
-    # Calculate probability
-    propability_function(Gdif[0])
-    return C, E, Gdif, a, phi, pst, segments, t
 
 
 if __name__ == "__main__":
